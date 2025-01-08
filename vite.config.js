328757322a22605e71685aac5159b97927da1f61
Florian Van Belleghem <florian.vanbelleghem@flux.be>
2025-01-08 14:44:05 +0100

    chore: enable source maps

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
 export default {
-    // Necessary for GH Pages since it's hosted on a subpath.
-    base: "debug-workshop",
-}
+  // Necessary for GH Pages since it's hosted on a subpath.
+  base: "debug-workshop",
+  build: {
+    sourcemap: true,
+  },
+};
